72ce12580942a22bef962707d6d6321885ddddef
Adrnikipad <detectivecameraman777@gmail.com>
2026-08-27 01:45:42 -0700

    Add files via upload

---
+++ ERROR 408/Form2.cs
@@ -0,0 +1,47 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ERROR_408.Form2
+// Assembly: ERROR 408, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: 3A16A069-E54E-486F-B90E-25ABCEBFADB0
+// Assembly location: \\VBOXSVR\B4s3\ToxicDatabase-main\BadJoke\ERROR 408\ERROR 408.7z\ERROR 408.exe
+// XML documentation location: \\VBOXSVR\B4s3\ToxicDatabase-main\BadJoke\ERROR 408\ERROR 408.7z\ERROR 408.xml
+
+using Microsoft.VisualBasic.CompilerServices;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+#nullable disable
+namespace ERROR_408;
+
+[DesignerGenerated]
+public class Form2 : Form
+{
+  private IContainer components;
+
+  public Form2() => this.InitializeComponent();
+
+  [DebuggerNonUserCode]
+  protected override void Dispose(bool disposing)
+  {
+    try
+    {
+      if (!disposing || this.components == null)
+        return;
+      this.components.Dispose();
+    }
+    finally
+    {
+      base.Dispose(disposing);
+    }
+  }
+
+  [DebuggerStepThrough]
+  private void InitializeComponent()
+  {
+    this.components = (IContainer) new System.ComponentModel.Container();
+    this.AutoScaleMode = AutoScaleMode.Font;
+    this.ClientSize = new Size(800, 450);
+    this.Text = nameof (Form2);
+  }
+}
---
+++ ERROR 408/My/MyComputer.cs
@@ -0,0 +1,25 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: ERROR_408.My.MyComputer
+// Assembly: ERROR 408, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: 3A16A069-E54E-486F-B90E-25ABCEBFADB0
+// Assembly location: \\VBOXSVR\B4s3\ToxicDatabase-main\BadJoke\ERROR 408\ERROR 408.7z\ERROR 408.exe
+// XML documentation location: \\VBOXSVR\B4s3\ToxicDatabase-main\BadJoke\ERROR 408\ERROR 408.7z\ERROR 408.xml
+
+using Microsoft.VisualBasic.Devices;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+
+#nullable disable
+namespace ERROR_408.My;
+
+[GeneratedCode("MyTemplate", "11.0.0.0")]
+[EditorBrowsable(EditorBrowsableState.Never)]
+internal class MyComputer : Computer
+{
+  [DebuggerHidden]
+  [EditorBrowsable(EditorBrowsableState.Never)]
+  public MyComputer()
+  {
+  }
+}
